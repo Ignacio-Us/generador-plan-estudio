7f340a5491b6e18dfcf48c91e12ce241a1b5114d
IGNACIO URIBE <i.uribe01@ufromail.cl>
2026-05-27 12:12:23 -0400

    test: Agregacion de assert para comprobar la configuracion de la API key

--- cypress/e2e/smoke_test_health.cy.js
+++ cypress/e2e/smoke_test_health.cy.js
@@ -6,6 +6,7 @@ describe('Smoke Test - Health Endpoint', () => {
       expect(response.status).to.eq(200);
       expect(response.body.status).to.eq('up');
       expect(response.body.message).to.eq('API esta funcionando correctamente');
+      expect(response.body.llm_connection).to.eq('API key configurada correctamente');
     });
   });
 });
