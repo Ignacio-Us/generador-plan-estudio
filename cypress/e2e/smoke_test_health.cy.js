8f20522cf42960db74321781da7481fb7bf25636
IGNACIO URIBE <i.uribe01@ufromail.cl>
2026-05-26 21:12:34 -0400

    test: Inicializacion cypress y prueba de health basico

---
+++ cypress/e2e/smoke_test_health.cy.js
@@ -0,0 +1,11 @@
+const baseUrl = 'http://localhost:3000';
+
+describe('Smoke Test - Health Endpoint', () => {
+  it('should return 200 status code', () => {
+    cy.request('GET', `${baseUrl}/health`).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.status).to.eq('up');
+      expect(response.body.message).to.eq('API esta funcionando correctamente');
+    });
+  });
+});
